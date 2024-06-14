b1f84a83ddf0564a8dadd54a23b79425f1157b18
Isabella <isabellamarcelino@gmail.com>
2024-06-14 10:20:59 -0300

    commitando projeto

---
+++ index.js
@@ -0,0 +1,199 @@
+const CamisaVerde = document.getElementById('CamisaVerde');
+
+CamisaVerde.addEventListener('mouseover', function(){
+ CamisaVerde.textContent = 'Unisex P ao Plus size';
+});
+
+CamisaVerde.addEventListener('mouseout', function(){
+   CamisaVerde.textContent = 'Camisa verde Mangas Curtas';
+});
+
+const CamisaPreta = document.getElementById('CamisaPreta');
+
+CamisaPreta.addEventListener('mouseover', function(){
+ CamisaPreta.textContent = 'Unisex P ao Plus size';
+});
+
+CamisaPreta.addEventListener('mouseout', function(){
+   CamisaPreta.textContent = 'Camisa Preta Floral';
+});
+
+const CamisaSalmao = document.getElementById('CamisaSalmao');
+
+CamisaSalmao.addEventListener('mouseover', function(){
+ CamisaSalmao.textContent = 'Unisex P ao Plus size';
+});
+
+CamisaSalmao.addEventListener('mouseout', function(){
+   CamisaSalmao.textContent = 'Camisa Preta Floral';
+});
+
+const CamisaXadrez = document.getElementById('CamisaXadrez');
+
+CamisaXadrez.addEventListener('mouseover', function(){
+ CamisaXadrez.textContent = 'Unisex P ao Plus size';
+});
+
+CamisaXadrez.addEventListener('mouseout', function(){
+   CamisaXadrez.textContent = 'Camisa Preta Floral';
+});
+
+
+const camisaTransparente = document.getElementById('camisaTransparente');
+
+camisaTransparente.addEventListener('mouseover', function(){
+ camisaTransparente.textContent = 'Unisex P ao Plus size';
+});
+
+camisaTransparente.addEventListener('mouseout', function(){
+   camisaTransparente.textContent = '$35.00';
+});
+
+
+const camisaLaranja = document.getElementById('camisaLaranja');
+
+camisaLaranja.addEventListener('mouseover', function(){
+ camisaLaranja.textContent = 'Unisex P ao Plus size';
+});
+
+camisaLaranja.addEventListener('mouseout', function(){
+   camisaLaranja.textContent = '$47.00';
+});
+
+const camisaPraiana = document.getElementById('camisaPraiana');
+
+camisaPraiana.addEventListener('mouseover', function(){
+ camisaPraiana.textContent = 'Unisex P ao Plus size';
+});
+
+camisaPraiana.addEventListener('mouseout', function(){
+   camisaPraiana.textContent = '$58.95';
+});
+
+const camisaSocial = document.getElementById('camisaSocial');
+
+camisaSocial.addEventListener('mouseover', function(){
+ camisaSocial.textContent = 'Unisex P ao Plus size';
+});
+
+camisaSocial.addEventListener('mouseout', function(){
+   camisaSocial.textContent = '$45.00';
+});
+
+const camisaColorida = document.getElementById('camisaColorida');
+
+camisaColorida.addEventListener('mouseover', function(){
+ camisaColorida.textContent = 'Unisex P ao Plus size';
+});
+
+camisaColorida.addEventListener('mouseout', function(){
+   camisaColorida.textContent = '$34.56';
+});
+
+const camisaBasica = document.getElementById('camisaBasica');
+
+camisaBasica.addEventListener('mouseover', function(){
+ camisaBasica.textContent = 'Unisex P ao Plus size';
+});
+
+camisaBasica.addEventListener('mouseout', function(){
+   camisaBasica.textContent = '$43.50';
+});
+
+const camisetaRedonda = document.getElementById('camisetaRedonda');
+
+camisetaRedonda.addEventListener('mouseover', function(){
+ camisetaRedonda.textContent = 'Unisex P ao Plus size';
+});
+
+camisetaRedonda.addEventListener('mouseout', function(){
+   camisetaRedonda.textContent = '$63.00';
+});
+
+const camisetaAlta = document.getElementById('camisetaAlta');
+
+camisetaAlta.addEventListener('mouseover', function(){
+ camisetaAlta.textContent = 'Unisex P ao Plus size';
+});
+
+camisetaAlta.addEventListener('mouseout', function(){
+   camisetaAlta.textContent = '$52.00';
+});
+
+const camisetaOver = document.getElementById('camisetaOver');
+
+camisetaOver.addEventListener('mouseover', function(){
+ camisetaOver.textContent = 'Unisex P ao Plus size';
+});
+
+camisetaOver.addEventListener('mouseout', function(){
+   camisetaOver.textContent = '$56.99';
+});
+
+const camisetaLisa = document.getElementById('camisetaLisa');
+
+camisetaLisa.addEventListener('mouseover', function(){
+ camisetaLisa.textContent = 'Unisex P ao Plus size';
+});
+
+camisetaLisa.addEventListener('mouseout', function(){
+   camisetaLisa.textContent = '$56.00';
+});
+
+const camisaFloral = document.getElementById('camisaFloral');
+
+camisaFloral.addEventListener('mouseover', function(){
+ camisaFloral.textContent = 'Unisex P ao Plus size';
+});
+
+camisaFloral.addEventListener('mouseout', function(){
+   camisaFloral.textContent = '$39.98';
+});
+
+const camisaListrada = document.getElementById('camisaListrada');
+
+camisaListrada.addEventListener('mouseover', function(){
+ camisaListrada.textContent = 'Unisex P ao Plus size';
+});
+
+camisaListrada.addEventListener('mouseout', function(){
+   camisaListrada.textContent = '$50.00';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById('botaoMais').addEventListener('click', function(){
+//     let maisConteudo = document.getElementById('maisConteudo');
+//     if (maisConteudo.style.display === 'none') {
+//         maisConteudo.style.display = 'block';
+//         this.textContent = 'Mostrar menos';
+//     } else {
+//         maisConteudo.style.display = 'none';
+//         this.textContent = 'Mostrar Mais'
+//     }
+// })
+
